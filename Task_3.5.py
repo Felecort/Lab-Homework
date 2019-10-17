42af40cc2d509b3d4126f41529dab2cdbc3accae
FriLDD <dotpointen@gmail.com>
2019-10-17 20:18:52 +0300

    done

--- Task_3.5.py
+++ Task_3.5.py
@@ -1,6 +1,15 @@
 #Напишите функцию, которая выполняет умножение двух числе с 
 #использованием только +, -, << (умножение на 2) и >> (деление на 2).
+
 def multiplication(a, b):
+#экспериментально установлено, для ускорения работы 
+#первое число должно быть меньше второго,
+#что и осуществляют следующие 4 строки кода
+	if a > b:
+		z = a
+		a = b
+		b = z
+	print(a, b)
 	bb = b
 	num = 0
 	c = 1
@@ -9,36 +18,29 @@ def multiplication(a, b):
 		num += 1
 	c = c >> 1
 	num = num - 1
-	b = b << num # b * 2^num
+	b = b << num   # b * 2^num
 	a -= c 
 	while a > 0:
 		b += bb
 		a -=1
 	return b 
+def calculator(a, b):
+	if a == 0 or b == 0:
+		return 0
+	elif a > 0 and b > 0:
+		return multiplication(a, b)
+	elif a < 0 and b < 0:
+		a = -a 
+		b = -b
+		return multiplication(a, b)
+	elif a < 0 or b < 0:
+		a = abs(a)
+		b = abs(a)
+		return -multiplication(a, b)
 
-a = 3472299622
-b = -564155
-if a == 0 or b == 0:
-	print("результат произведения = 0")
-elif a < 0 or b < 0:
-	t = -1
-	a = abs(a)
-	b = abs(b)
-	bb = b
-	if a < b:
-		q = multiplication(a,b)
-		print("результат произведения =", q-q-q)
-	else:
-		z = a
-		a = b
-		b = z
-		q = multiplication(a,b)
-		print("результат произведения =", q-q-q) 
-else:
-	if a < b:
-		print("результат произведения =",multiplication(a, b))
-	else:
-		z = a
-		a = b
-		b = z
-		print("результат произведения =",multiplication(a, b))
+
+		
+print("результат произведеня =", calculator(50000, 200))
+
+
+
