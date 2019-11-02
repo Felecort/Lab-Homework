95325dfef619d9cd527ee368f29417fbb3059859
FriLDD <dotpointen@gmail.com>
2019-11-02 13:23:52 +0300

    05.11

--- Task_2.1.5.py
+++ Task_2.1.5.py
@@ -1,32 +1,28 @@
 # готовую библиотечную реализацию квадратного корня использовать 
 # nельзя. Напишите функцию, которая вычисляет 
 # квадратный корень вещественного числа с помощью поиска.
-def calculator(a):
-	#проверка, являчется ли число положительным 
-	assert a >= 0
-	#определение количества знаков после запятой
-	s = str(a)
-	if '.' in s:
-	    length =  abs(s.find('.') - len(s)) - 1
-	else: 
-		length = 0 
-	if length % 2 != 0:
-		length += 1
-	#перевод вещественного числа в натуральное
-	a10 = a * (10 ** length)
-	a10 = int(a10)
-	#вычисление корня с помощью поиска
-	j = 0
-	sqrt = 0
-	while j != 1:
-		if (sqrt * sqrt)<= a10 and ((sqrt + 1) * (sqrt + 1)) > a10:
-			j = 1
-		else: sqrt += 1
-	sqrt = sqrt / ((10 ** (length / 2)))
-	return sqrt
-
-print(calculator(5000000))
-print(calculator(455.1554))
-print(calculator(56496))
-print(calculator(56444.564879))
-print(calculator(123.5164889))
+import math
+def Root_Calculation(integer):
+	if integer < 0:
+		return "число меньше 0"
+	degree = 0
+	if integer % 1 != 0:
+		while integer % 1 > 0:
+			integer = integer * 10
+			degree += 1
+	if degree % 2 != 0:
+		degree += 1
+		integer *= 10
+	degree /= 2
+	low = 0
+	high = integer // 2
+	while low <= high:
+		root = (low + (high - low) // 2)
+		if root ** 2 == integer:
+			return root / (10 ** degree)
+		elif root ** 2 < integer:
+			low = root + 1
+		else:
+			high = root - 1
+	return root / (10 ** degree)
+print(Root_Calculation(458645465.456454646))
